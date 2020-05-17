5af0517b962794d5dc6b909cee222355879f75a7
Mirko Bleyh <mirkoogle@gmail.com>
2020-05-17 11:09:35 +0200

    Removed stocks from config

--- config/config.js
+++ config/config.js
@@ -300,24 +300,24 @@ var config = {
 				maxNumberOfDays: 10
 			}
 		},
-		{
-			module: "MMM-AVStock",
-			position: "lowerthird_right", //"bottom_bar" is better for `mode:ticker`
-			config: {
-				apiKey : "E8EASNE65KU2212J", // https://www.alphavantage.co/
-				timeFormat: "YYYY-MM-DD HH:mm:ss",
-				symbols : ["18MF.DEX", "LYPG.DEX", "SAP", "IFX.DEX", "NQ64.STG", "OG7T.FRK"],
-				alias: ["Amundi ETF", "Lyxor ETF", "SAP", "Infineon", "nordasia.com", "DEKA ETF"], //Easy name of each symbol. When you use `alias`, the number of symbols and alias should be the same. If value is null or "", symbol string will be used by default.
-				tickerDuration: 300, // Ticker will be cycled once per this second.
-				chartDays: 90, //For `mode:series`, how much daily data will be taken. (max. 90)
-				poolInterval : 1000*15, // (Changed in ver 1.1.0) - Only For Premium Account
-				mode : "table", // "table", "ticker", "series"
-				decimals: 1, // number o decimals for all values including decimals (prices, price changes, change%...)
-				candleSticks : false, //show candle sticks if mode is Series
-				coloredCandles : false, //colored bars: red and green for negative and positive candles
-				premiumAccount: false, // To change poolInterval, set this to true - Only For Premium Account
-			}
-		},
+		// {
+		// 	module: "MMM-AVStock",
+		// 	position: "lowerthird_right", //"bottom_bar" is better for `mode:ticker`
+		// 	config: {
+		// 		apiKey : "E8EASNE65KU2212J", // https://www.alphavantage.co/
+		// 		timeFormat: "YYYY-MM-DD HH:mm:ss",
+		// 		symbols : ["18MF.DEX", "LYPG.DEX", "SAP", "IFX.DEX", "NQ64.STG", "OG7T.FRK"],
+		// 		alias: ["Amundi ETF", "Lyxor ETF", "SAP", "Infineon", "nordasia.com", "DEKA ETF"], //Easy name of each symbol. When you use `alias`, the number of symbols and alias should be the same. If value is null or "", symbol string will be used by default.
+		// 		tickerDuration: 300, // Ticker will be cycled once per this second.
+		// 		chartDays: 90, //For `mode:series`, how much daily data will be taken. (max. 90)
+		// 		poolInterval : 1000*15, // (Changed in ver 1.1.0) - Only For Premium Account
+		// 		mode : "table", // "table", "ticker", "series"
+		// 		decimals: 1, // number o decimals for all values including decimals (prices, price changes, change%...)
+		// 		candleSticks : false, //show candle sticks if mode is Series
+		// 		coloredCandles : false, //colored bars: red and green for negative and positive candles
+		// 		premiumAccount: false, // To change poolInterval, set this to true - Only For Premium Account
+		// 	}
+		// },
 		{
   		module: 'MMM-Sonos',
   		position: 'middle_center', // you may choose any location
